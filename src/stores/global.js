b6c3a6d196434ba891b956cdeb951fd8aa20ea90
alvaro <alvarofdayala@gmail.com>
2024-12-12 13:21:08 +0100

    env

--- src/stores/global.js
+++ src/stores/global.js
@@ -1,11 +1,10 @@
 import { defineStore } from 'pinia'
 
 export const globalStore = defineStore('global', () => {
-  const API_URL = 'https://ratespotserver.onrender.com'
-  // const API_URL = 'http://localhost:3000'
-  const USER_LOCALSTORAGE_KEY = 'albondiga_frita'
-  const GROUP_LOCALSTORAGE_KEY = 'salamance_alpinero'
-  const GROUP_LOCALSTORAGE_SALT = '647280043212'
+  const API_URL = import.meta.env.VITE_API_URL
+  const USER_LOCALSTORAGE_KEY = import.meta.env.VITE_USER_LOCALSTORAGE_KEY
+  const GROUP_LOCALSTORAGE_KEY = import.meta.env.VITE_GROUP_LOCALSTORAGE_KEY
+  const GROUP_LOCALSTORAGE_SALT = import.meta.env.VITE_GROUP_LOCALSTORAGE_SALT
 
   return {
     API_URL,
